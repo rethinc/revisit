f4c7f1990c9168c5659a1bf0805903dd9d0f68ad
Yves Bonjour <yves.bonjour@rethinc.ch>
2020-05-09 09:48:41 +0200

    Adds basic Android project

---
+++ android/app/src/main/java/ch/rethinc/gastrocheckin/MainActivity.kt
@@ -0,0 +1,12 @@
+package ch.rethinc.gastrocheckin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
