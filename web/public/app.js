8f2f669197714076274dd884b09c0e93719f48e2
Yves Bonjour <yves.bonjour@rethinc.ch>
2020-05-09 17:38:27 +0200

    Adds possibility to remove visit

--- web/public/app.js
+++ web/public/app.js
@@ -8,27 +8,48 @@
   const containerVisits = document.getElementById('containerVisits')
   const buttonSignOut = document.getElementById('buttonSignOut')
 
-  var visitsTable = new Tabulator("#containerVisits", {
-    layout:"fitColumns",
-    index: "id",
+  var currentUser = null
+
+  var currentData = new Set([])
+
+  var deleteIcon = (cell, formatterParams) => {
+    return '<svg class="bi bi-trash" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">\n' +
+      '  <path d="M5.5 5.5A.5.5 0 016 6v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm2.5 0a.5.5 0 01.5.5v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm3 .5a.5.5 0 00-1 0v6a.5.5 0 001 0V6z"/>\n' +
+      '  <path fill-rule="evenodd" d="M14.5 3a1 1 0 01-1 1H13v9a2 2 0 01-2 2H5a2 2 0 01-2-2V4h-.5a1 1 0 01-1-1V2a1 1 0 011-1H6a1 1 0 011-1h2a1 1 0 011 1h3.5a1 1 0 011 1v1zM4.118 4L4 4.059V13a1 1 0 001 1h6a1 1 0 001-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" clip-rule="evenodd"/>\n' +
+      '</svg>'
+  }
+
+
+  var visitsTable = new Tabulator('#containerVisits', {
+    layout: 'fitColumns',
+    index: 'id',
     columns: [
-      {title: "Name", field: "name"},
-      {title: "Telefonnummer", field: "phone"},
-      {title: "Datum", field: "visitedAt"}
+      {title: 'Name', field: 'name', editor: 'input'},
+      {title: 'Telefonnummer', field: 'phone'},
+      {title: 'Datum', field: 'visitedAt'},
+      {
+        formatter: deleteIcon, width: 44, hozAlign: 'center', cellClick: (e, cell) => {
+          if (currentUser) {
+            deleteVisit(cell.getRow().getData().id, currentUser.uid)
+          }
+        }
+      }
     ]
-  });
+  })
 
   var loadVisitsUnsubscribe = null
 
   auth.onAuthStateChanged(firebaseUser => {
     if (firebaseUser) {
       console.log(firebaseUser)
+      currentUser = firebaseUser
       showSignedInView(firebaseUser)
     } else {
+      currentUser = null
       console.log('not logged in')
       showSignInView()
     }
-  });
+  })
 
   function showSignedInView(firebaseUser) {
     containerSignIn.classList.add('hide')
@@ -57,16 +78,28 @@
       credentialHelper: firebaseui.auth.CredentialHelper.NONE,
       callbacks: {
         signInSuccessWithAuthResult: (authResult, redirectUrl) => {
-          return false;
+          return false
         }
       }
-    });
+    })
   }
 
   function signOut() {
-      auth.signOut()
+    auth.signOut()
   }
 
+  function deleteVisit(visitId, userId) {
+
+    return db
+      .collection('places')
+      .doc(userId)
+      .collection('visits')
+      .doc(visitId)
+      .delete()
+      .catch(error =>
+        console.error(error.message)
+      )
+  }
 
   function loadVisits(db, userId) {
     return db
@@ -83,11 +116,12 @@
             phone: docData.phone,
             visitedAt: docData.visitedAt
           })
-        });
-        visitsTable.setData(visits);
+        })
+        currentData = new Set(visits)
+        visitsTable.setData(visits)
       }, error => {
         console.log(error.message)
-      });
+      })
   }
 
 }())
