839da5188542994a31cd1587ae873614c06a1cf3
Yves Bonjour <yves.bonjour@rethinc.ch>
2020-05-09 18:11:23 +0200

    Allows to add visit and formats date

--- web/public/app.js
+++ web/public/app.js
@@ -7,12 +7,14 @@
   const containerSignIn = document.getElementById('containerSignIn')
   const containerVisits = document.getElementById('containerVisits')
   const buttonSignOut = document.getElementById('buttonSignOut')
+  const containerCreate = document.getElementById('containerCreate')
+  const textName = document.getElementById('textName')
+  const textPhone = document.getElementById('textPhone')
+  const buttonCreate = document.getElementById('buttonCreate')
 
   var currentUser = null
 
-  var currentData = new Set([])
-
-  var deleteIcon = (cell, formatterParams) => {
+  const deleteIcon = (cell, formatterParams) => {
     return '<svg class="bi bi-trash" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">\n' +
       '  <path d="M5.5 5.5A.5.5 0 016 6v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm2.5 0a.5.5 0 01.5.5v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm3 .5a.5.5 0 00-1 0v6a.5.5 0 001 0V6z"/>\n' +
       '  <path fill-rule="evenodd" d="M14.5 3a1 1 0 01-1 1H13v9a2 2 0 01-2 2H5a2 2 0 01-2-2V4h-.5a1 1 0 01-1-1V2a1 1 0 011-1H6a1 1 0 011-1h2a1 1 0 011 1h3.5a1 1 0 011 1v1zM4.118 4L4 4.059V13a1 1 0 001 1h6a1 1 0 001-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" clip-rule="evenodd"/>\n' +
@@ -26,7 +28,11 @@
     columns: [
       {title: 'Name', field: 'name', editor: 'input'},
       {title: 'Telefonnummer', field: 'phone'},
-      {title: 'Datum', field: 'visitedAt'},
+      {
+        title: 'Datum', field: 'visitedAt', formatter: (cell, formatterParams) => {
+          return new Date(cell.getValue()).format('d.m.Y H:i')
+        }
+      },
       {
         formatter: deleteIcon, width: 44, hozAlign: 'center', cellClick: (e, cell) => {
           if (currentUser) {
@@ -41,12 +47,10 @@
 
   auth.onAuthStateChanged(firebaseUser => {
     if (firebaseUser) {
-      console.log(firebaseUser)
       currentUser = firebaseUser
       showSignedInView(firebaseUser)
     } else {
       currentUser = null
-      console.log('not logged in')
       showSignInView()
     }
   })
@@ -54,6 +58,8 @@
   function showSignedInView(firebaseUser) {
     containerSignIn.classList.add('hide')
     containerSignIn.classList.add('hide')
+    containerCreate.classList.remove('hide')
+    buttonCreate.addEventListener('click', createVisitFromForm)
     containerVisits.classList.remove('hide')
     buttonSignOut.classList.remove('hide')
     buttonSignOut.addEventListener('click', signOut)
@@ -62,12 +68,15 @@
 
   function showSignInView() {
     containerSignIn.classList.remove('hide')
+    containerCreate.classList.add('hide')
+    buttonCreate.removeEventListener('click', createVisitFromForm)
     containerVisits.classList.add('hide')
     if (loadVisitsUnsubscribe) {
       loadVisitsUnsubscribe()
     }
     buttonSignOut.classList.add('hide')
     buttonSignOut.removeEventListener('click', signOut)
+    buttonCreate.removeEventListener('click', signOut)
     authUi.start('#containerSignIn', {
       signInOptions: [
         {
@@ -84,13 +93,40 @@
     })
   }
 
+  function createVisitFromForm() {
+    if (currentUser && textName.value && textPhone.value) {
+      createVisit(textName.value, textPhone.value, currentUser.uid)
+    }
+  }
+
   function signOut() {
     auth.signOut()
   }
 
-  function deleteVisit(visitId, userId) {
+  function createVisit(name, phone, userId) {
+    let visit = {
+      id: uuidv4(),
+      name: name,
+      phone: phone,
+      visitedAt: Date.now()
+    }
+    db
+      .collection('places')
+      .doc(userId)
+      .collection('visits')
+      .doc(visit.id)
+      .set(visit)
+      .then(() => {
+        textPhone.value = ''
+        textName.value = ''
+      })
+      .catch(error =>
+        console.error(error.message)
+      )
+  }
 
-    return db
+  function deleteVisit(visitId, userId) {
+    db
       .collection('places')
       .doc(userId)
       .collection('visits')
