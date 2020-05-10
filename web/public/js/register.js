12aa040d06e28e62a3be215a5dd128d23b473d01
krigu <christian.feuz@rethinc.ch>
2020-05-10 14:30:04 +0200

    Adds basic registration.html

---
+++ web/public/js/register.js
@@ -0,0 +1,47 @@
+function generateQrCode (name, phone) {
+
+  const typeNumber = 4
+  const errorCorrectionLevel = 'L'
+  const qr = qrcode(typeNumber, errorCorrectionLevel)
+  const qrCodeContent = {
+    'name': name,
+    'phone': phone
+  }
+  qr.addData(JSON.stringify(qrCodeContent))
+  qr.make()
+  const qrCodeCellSize = 20
+  const qrCodeMargin = 20
+  document.getElementById('qrCode').innerHTML = qr.createImgTag(
+    qrCodeCellSize,
+    qrCodeMargin,
+    'QrCode'
+  )
+}
+
+function hideRegistrationForm() {
+  document.getElementById('registrationForm').style.display = 'none';
+}
+
+function isUserDataPresent () {
+  const queryString = window.location.search
+  const urlParams = new URLSearchParams(queryString)
+  const name = urlParams.get('name')
+  const phone = urlParams.get('phone')
+
+  if (name && phone) {
+    generateQrCode(name, phone)
+    hideRegistrationForm()
+  }
+}
+
+if (document.readyState !== 'loading') {
+  isUserDataPresent()
+} else {
+  document.addEventListener('DOMContentLoaded', function () {
+    isUserDataPresent()
+  })
+}
+
+
+
+
