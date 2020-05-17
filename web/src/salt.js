4ddaba887b866f42c9973c8cbba53fca60a032e6
Yves Bonjour <yves.bonjour@rethinc.ch>
2020-05-17 12:20:12 +0200

    Removes debug log output

--- web/src/salt.js
+++ web/src/salt.js
@@ -2,7 +2,6 @@ const encryption = require('./encryption.js')
 const firebase = require('./revisit.firebase.js')
 
 module.exports.getOrCreate = function (userId) {
-  console.log(userId)
   return getSalt(userId)
     .then((salt) => {
       if (salt) {
