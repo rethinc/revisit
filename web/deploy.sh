b29fdf6cb1c695a98a93261fb122dbd4dfbe4bbe
Yves Bonjour <yves.bonjour@rethinc.ch>
2021-05-17 11:43:08 +0200

    Updates dependencies of functions

--- web/deploy.sh
+++ web/deploy.sh
@@ -7,20 +7,21 @@ set -o nounset
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 main() {
-
-  npm=$(which npm)
+  npm=$(which npm || true)
   if [ -z "${npm:-}" ]; then
     echo  "You need to install Node"
     exit 1
   fi
 
-	browserify=$(which browserify)
+	browserify=$(which browserify || true)
 	if [ -z "${browserify:-}" ]; then
 	  echo "You need to install browserify run `npm install -g browserify`"
 	  exit 1
   fi
 
-	firebase=$(which firebase)
+  echo "firebase"
+
+	firebase=$(which firebase || true)
 	if [ -z "${firebase:-}" ]; then
 	  echo "You need to install firebase run `npm install -g firebase-tools`"
 	  exit 1
@@ -31,6 +32,7 @@ main() {
      echo "Please add ${dir}/src/restaurant/firebase.config.js from rethinc-signing repo."
      exit 1
     }
+
     ${npm} install
     ${browserify} restaurant.js -o ../../public/js/restaurant.js
   popd
@@ -42,5 +44,6 @@ main() {
   ${firebase} login
   ${firebase} deploy
 }
+
 
 main "$@"
